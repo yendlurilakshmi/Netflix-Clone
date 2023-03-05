c83885e527c4c6749a52cbc8f5731b30b81fc919
yendlurilakshmi <yendlurilakshmi18@gmail.com>
2023-03-05 16:06:18 +0530

    checkpoint3

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -5,13 +5,39 @@ import requests from "../../request";
 
 const Banner = () => {
   const [movie, setMovie] = useState([]);
-  useEffect(() => {
+  async function fetchDataFromURL() {
+    let output = await axios.get(requests.fetchNetflixOriginals);
+    let movies = output.data.results;
+    let randomNumber = Math.floor(Math.random() * movies.length);
+    setMovie(movies[randomNumber]);
+  }
 
+
+  useEffect(() => {
+    fetchDataFromURL();
   }, []);
 
-  return (
+  
+  const backgroundStyle = {
+    backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`,
+    backgroundPosition: 'center center',
+    backgroundSize: 'cover',
+  };
 
-  );
+  return (
+    <header className="banner" style={backgroundStyle}>
+      <div className="banner__contents">
+        <h1 className="banner__title">{movie.name}</h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <p className="banner__description">{movie.overview}</p>
+
+      </div>
+    </header>
+
+  )
 };
 
 export default Banner;
